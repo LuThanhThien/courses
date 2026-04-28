240ee653d5be1e5b2dd84c42ada3c4aad90b80cd
LuThanhThien <luthien5921@gmail.com>
2026-04-28 23:04:05 +0900

    FIX: refractor src base

--- Programming_Languages/cpp/CMake/src/MathLib/cpp/MathLib.cpp
+++
@@ -1,6 +0,0 @@
-#include "MathLib.h"
-
-namespace MathLib {
-	int add(int a, int b) { return a + b; }
-	float add(float a, float b) { return a + b; }
-}
--- Programming_Languages/cpp/CMake/src/MathLib/cpp/MathLib.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-namespace MathLib {
-	int add(int a, int b);
-	float add(float a, float b);
-}
